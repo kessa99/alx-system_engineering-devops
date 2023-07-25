62d1110277998f4188358be52dc10a63de3e03c9
kessa99 <kessadavidkipre@gmail.com>
2023-07-24 20:08:14 -0700

    How use API REST

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        user = sys.argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        rpse = requests.get("{}users/{}".format(url, user))
+        name = rpse.json().get("name")
+
+        if name is not None:
+            scd_rpse =
+            requests.get("{}todo?userId={}".format(url, user)).json()
+            all_task = len(scd_rpse)
+            done_task = []
+
+            for task in scd_rpse:
+                if task.get("completed") is True:
+                    done_task.append(task)
+            count = len(done_task)
+            print("Employee {} is done with tasks({}/{}): ".format(name, count, done_task))
+            for title in done_task:
+                print("\t {}".format(title.get("title")))
