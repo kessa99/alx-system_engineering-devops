05420cbd563df93a4a71dda7966ef60e6bc51754
kessa99 <kessadavidkipre@gmail.com>
2023-07-26 21:57:15 -0700

    Gather data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,27 +3,46 @@
 """
 Script python with API Rest to import information.
 """
+
+import json
 import requests
 import sys
 
 if __name__ == "__main__":
+    """
+    Step1 Connection to API
+    """
     if len(sys.argv) > 1:
-        user = sys.argv[1]
+        user = int(sys.argv[1])
         url = "https://jsonplaceholder.typicode.com/"
-        rpse = requests.get("{}users/{}".format(url, user))
-        name = rpse.json().get("name")
+        response_API = requests.get("{}users/{}".format(url, user))
+        """
+        Check if the status code is 200
+        """
+        if response_API.status_code == 200:
+            """
+                Obtain response_API in json format
+            """
+            data = response_API.text
+            json_format = json.loads(data)
+            name = json_format.get("name")
 
-        if name is not None:
-            scd_rpse = requests.get("{}todo?userId={}".format(url,
-                                    user)).json()
-            all_task = len(scd_rpse)
-            done_task = []
+            if name is not None:
+                """
+                check if name variable is not empty
+                """
+                second_rpse = requests.get("{}todo?userId={}"
+                                           .format(url, user))
+                data1 = second_rpse.text
+                second_json_format = json.loads(data1)
+                all_task = len(second_json_format)
+                done_task = []
 
-            for task in scd_rpse:
+            for task in second_json_format:
                 if task.get("completed") is True:
                     done_task.append(task)
             count = len(done_task)
             print("Employee {} is done with tasks({}/{}): ".format(name,
-                  count, done_task))
+                  count, all_task))
             for title in done_task:
                 print("\t {}".format(title.get("title")))
