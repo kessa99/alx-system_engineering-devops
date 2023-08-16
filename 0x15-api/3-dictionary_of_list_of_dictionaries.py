8a0d9d203248a9dc2362481abc6f082a7de05e73
kessa99 <kessadavidkipre@gmail.com>
2023-08-16 01:28:48 -0700

    many save

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,7 +3,32 @@
 """
 """
 
+import json
+import requests
+import sys
+
 if __name__ == "__main__":
-    import json
-    import requests
-    import sys
+    param = sys.argv[1]
+
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(param)
+    response_json = requests.get(url, verify=True).json()
+
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(param)
+    todo_json = requests.get(url, verify=True).json()
+
+    user_id = "{}".format(param)
+    user_info = {
+        user_id: []
+    }
+
+    for task in todo_json:
+        task_info = {
+            "username": task.get("username"),
+            "task": task.get("task"),
+            "completed": task.get("completed")
+        }
+
+    user_info[user_id].append(task_info)
+
+    with open("{}.json".format(param), 'w') as file:
+        json.dump(user_info, file)
